93fad159142fce2aca6d77d4a9e8edda0b3093e3
SylejmanDurguti <durgutisylejman00@gmail.com>
2024-10-10 19:11:37 +0200

    Initial commit

---
+++ Module_2/index.php
@@ -0,0 +1,80 @@
+<?php
+
+   $school = 'Digital School';
+
+   // echo "I love $school";
+
+// int, float, string, boolean, array, object, resources, null //
+
+
+//simple arithmetics +, -, /, *, %
+
+
+$x = 132;
+$y = 13;
+echo $x + $y;
+
+$z = 203;
+$b = 143;
+echo $z - $b;
+
+$c = 15;
+$a = 19;
+echo $c * $a;
+
+$k = 32;
+$l = 2;
+echo $k / $l;
+
+
+
+$a = 'Digital';
+
+$b = "School";
+$c = $a.$b;
+
+ echo "$c \n "
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
