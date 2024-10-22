385f8ed55a189e162f75610c7009683079da327d
Sylejman540 <durgutisylejman00@gmail.com>
2024-10-22 18:36:03 +0200

    Add files via upload

--- Module_4/index.php
+++ Module_4/index.php
@@ -7,70 +7,94 @@
 $num = -1;
 
 
-if($num < 0){  
-    echo "$num is greater than 0";
-}
+if ($num <0) { echo "$num is less than0"; // Changed 'greater' to 'less'  
+}  
+
+$age =144;  
+if ($age >18) {  
+ echo "You are a grandfather (wow)"; // Added a space before (wow)  
+} else {  
+ echo "You are under18 <br>";  
+}  
+
+if (($age >12) && ($age <20)) {  
+ echo "Discount for you!!!";  
+}  
+
+if ($num <0) {  
+ echo "The value of $num is negative";  
+}elseif ($num ==0) {  
+ echo "The value of $num is0";  
+} else {  
+ echo "The value of $num is a positive number"; // Changed 'pos' to 'positive'  
+}  
+
+$score =12;  
+
+if ($score >=90 && $score <=100) {  
+ echo "A";  
+} elseif ($score >=70 && $score <=89) {  
+ echo "B";  
+} elseif ($score >=50 && $score <=69) {  
+ echo "C";  
+} else {  
+ echo "F";  
+}  
+
+$grade = 'B';  
+
+switch ($grade) {  
+ case 'A': // Changed ';' to ':'  
+ echo 'Excellent! You got an A';  
+ break;  
+ case 'B': // Changed ';' to ':'  
+ echo 'Excellent! You got a B';  
+ break;  
+ case 'C': // Changed ';' to ':'  
+ echo 'Well done! You got a C!';  
+ break;  
+ case 'D': // Changed ';' to ':'  
+ echo 'Study more next time! You got a D';  
+ break;  
+ case 'F': // Changed ';' to ':'  
+ echo 'You failed! You got an F';  
+ break;  
+ default:  
+ echo 'Invalid value';  
+ break;  
+}  
+
+for ($x = 0; $x <= 10;$x++){
+    echo "The number is $x <br>";
+    }
+
+$x = 1;
+do {
+    echo "The number is $x<br>";
+    $x++;
+} while ($x <= 5);
+
+$x = 1;
+while ($x <= 5) {
+    echo "The number is $x<br>";
+    $x++;
+};
+
+$car = array("BMW", "VW", "Audi", "Tesla");
+    foreach($car as $value){
+    echo "$value <br>";
+    }
+
+    $age = array("John" => "18", "Michael" => "23", "Joe" => "10");
+    foreach($age as $x=> $value){
+    echo "$x=$val <br>";
+    }
 
 
-$age = 144;
-if($age > 18){
-    echo "You are a grandfather(wow)";
-}else{
-    echo "You are under 18 <br>";
-}
-
-if(($age > 12)  && ($age < 20)){
-    echo "Discount for you!!!";
-}
-
-// 
-if($num<0){
-    echo "The value of $num is negative";
-}elseif($num == 0){
-    echo "The value of $num is 0";
-}else{
-    echo "The value of $num is a pos number";
-}
-
-
-$score = 12;
-
-if($score >= 90 && $score <= 100){
-    echo "A";
-}elseif($score >=70 && $score <=89){
-    echo "B";
-}
-elseif($score >=50 && $score <=69){
-    echo "C";
-}
-else {
-    echo "F";
-}
 
 
 
 
-$grade = 'B';
 
-switch($grade){
-    case 'A';
-        echo 'Excellent! You got an A';
-    break;
-    case 'B';
-        echo 'Excellent! You got a B';
-    break;
-    case 'C';
-        echo 'Well done! You got a C!';
-    break;
-    case 'D';
-        echo 'Study more next time! You got a D';
-    break;
-    case 'F';
-        echo 'You failed! You got a F';
-    break;
-    default:
-    echo 'Invalid value';
-    break;
-}
+?>  
 
-?>
