6be5b94006da40be8178e62ec910d60e446cb5f1
Sylejman540 <durgutisylejman00@gmail.com>
2024-10-22 18:36:28 +0200

    Add files via upload

--- Module_3/index.php
+++ Module_3/index.php
@@ -1,16 +1,16 @@
 <?php
    $the_string = 'Digital School';
 
-   // Correct way to use strlen
+   
    echo strlen($the_string . "\n");
 
    $programming = 'Programming is not cool';
 
-   // Correct way to use str_replace
+   
    echo str_replace("not", "very", $programming);
   
    $another_string = 'Programming';
    
-   // Correct way to use strrev
+   
    echo strrev($another_string);
 ?>
