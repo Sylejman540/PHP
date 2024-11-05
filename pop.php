542db081425a01b40bde44c84236a95f93d7d8c8
Sylejman540 <durgutisylejman00@gmail.com>
2024-11-05 17:58:21 +0100

    Delete pop.php

--- pop.php
+++
@@ -1,6 +0,0 @@
-<?php
-$fruits = array('Apple', 'Banana', 'Orange');
-$lastFruit = array_pop($fruits);
-echo $lastFruit; // Output: Orange
-print_r($fruits); // Output: Array ( [0] => Apple [1] => Banana )
-?>
