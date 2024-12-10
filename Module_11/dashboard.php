29fa422d4afa2edc145b3780e6f1fc40e2ec006f
SylejmanDurguti <durgutisylejman00@gmail.com>
2024-12-10 19:18:11 +0100

    Update dashboard.php

--- Module_11/dashboard.php
+++ Module_11/dashboard.php
@@ -16,11 +16,55 @@ $users = $stmt->fetchAll(PDO::FETCH_ASSOC)
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>User Dashboard</title>
-</head>
-
     <style>
-            /* CSS */
+         <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 20px;
+            padding: 0;
+            background-color: #f9f9f9;
+        }
+        h2 {
+            color: #333;
+            text-align: center;
+        }
+        table {
+            width: 80%;
+            margin: 0 auto;
+            border-collapse: collapse;
+            background-color: #fff;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+        }
+        th, td {
+            text-align: left;
+            padding: 12px;
+            border: 1px solid #ddd;
+        }
+        th {
+            background-color: #C1BAA1;
+            color: white;
+        }
+        tr:nth-child(even) {
+            background-color: rgb(236, 235, 222);
+        }
+        tr:hover {
+            background-color: rgb(236, 235, 230);
+        }
+        a {
+            text-decoration: none;
+            color: rgb(165, 157, 132);
+        }
+        a:hover {
+            color: rgb(170, 157, 132);
+            text-decoration: underline;
+        }
+        .actions a {
+            margin-right: 10px;
+        }
+    </style> 
+ 
     </style>
+</head>
 
 <body>
         <h2>Users Dashboard</h2>
@@ -32,7 +76,17 @@ $users = $stmt->fetchAll(PDO::FETCH_ASSOC)
                 <th>Actions</th>
             <tr>
         <tbody>
-            <!--  -->
+          <?php foreach($users as $user)?>
+        <tr>
+         <td><?php echo htmlspecialchars($user ['username']);?></td>
+         <td><?php echo htmlspecialchars($user ['email']);?></td>
+         <td class="actions">
+            <a href="edit.php?id=<?=$user['id']; ?>">Edit</a>
+            <a href="">Delete</a>
+
+
+        </tr>
+        <?php endforeach; ?> 
 </tbody>
 <thead>
 </table>
