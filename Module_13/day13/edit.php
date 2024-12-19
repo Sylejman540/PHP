9c027e41b0d530102ef2403dd2b6b976d5e64051
SylejmanDurguti <durgutisylejman00@gmail.com>
2024-12-19 19:20:48 +0100

    Update edit.php

--- Module_13/day13/edit.php
+++ Module_13/day13/edit.php
@@ -1,4 +1,104 @@
+<?php 
+require_once "config.php";
+session_start();
+
+if(isset($_SESSION['admin_logged_in'])){
+    header("Location: login.php");
+    exit;
+}
+
+if(isset($_GET['id']) || empty($_GET['id'])){
+    header("Location : admin.php");
+    exit;
+}
+
+$id = intval($_GET['id']);
+$error = "";
+$sucess = "";
+
+// Fetch user details
+$sql = "SELECT * FROM users WHERE id = $id";
+$result = $conn->query($sql);
+
+if($result->num_rows == 1){
+    $user = $result->fetch_assoc();
+}else{
+    header("Location : admin.php");
+    exit;
+}
+
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $username = mysqli_real_espace_string($conn, $_POST['username']);
+    $email = mysqli_real_espace_string($conn, $_POST['email']);
+    
+    
+    if(empty($username) || empty($email){
+        $error = "All fields are required"
+    }
+
+}else{
+    $update_sql = "UPDATE users SET username = '$username', email = '$email' WHERE id = '$id'";
+    if($conn->query($update_sql)){
+        $success = "Username update successfully!";
+        // Refresh the user data
+        $user['username'] = $username;
+        $user['email'] = $email;
+    }else{
+        $error = "Error updating user:".$conn->error();
+    }
+}
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 <?php require_once("header.php"); ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
